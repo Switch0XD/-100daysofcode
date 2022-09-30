dc2585189a6c075020b2f00efa6029abf3417b55
Kuldeep Singh <switch2ks@gmail.com>
2022-09-30 23:23:50 +0530

    Day 1

---
+++ Day_1/DataTypes.java
@@ -0,0 +1,20 @@
+package Day_1;
+
+public class DataTypes {
+  public static void main(String[] args) {
+    // Primitives
+
+    int I_range = 2147483647; // -2,147,483,648 to 2,147,483,647
+    float Fl_range = 1.222222f; // Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+    double D_range = 1.123456789012345D; // Stores fractional numbers. Sufficient for storing 15 decimal digits
+    short S_range = 32767; // 32768 to 32767
+    long L_range = 9223372036854775807L; // -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+    byte range = 128; // -128 to 127
+
+    char Char_range = 127; // -128 to 127
+
+    boolean flag = true;
+    System.out.println(flag); // 1 bit
+  }
+}
